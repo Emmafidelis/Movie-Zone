268a021259e5cbefd198e4193a19870ab88f575c
Emanuel Fidelis <emanuelkagombora28@gmail.com>
2024-11-01 12:54:58 +0300

    Build a responsive layout

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,13 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { ChakraProvider } from "@chakra-ui/react";
+import { Provider } from "./components/ui/provider.tsx";
 import App from "./App.tsx";
 import "./index.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <ChakraProvider>
+    <Provider>
       <App />
-    </ChakraProvider>
+    </Provider>
   </StrictMode>
 );
